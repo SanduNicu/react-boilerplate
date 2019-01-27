6bbeced15f84433d3108e45ab79469ea3219e75f
3x3cu7or <sandu.nicu94@gmail.com>
2019-01-27 16:31:40 +0200

    added basic boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+import "styles/index.scss";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Boilerplate</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index-bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader", // creates style nodes from JS strings
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader" // compiles Sass to CSS, using Node Sass by default
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ],
+  resolve: {
+    modules: [path.resolve("./src"), path.resolve("./node_modules")]
+  }
+};
